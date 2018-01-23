0db1ac5d57826caad35d600c949bb3d1e624dab5
jonathan.essex <jonathan.essex@misys.com>
2018-01-23 17:35:55 +0000

    added parameters to runme

--- runme.sh
+++ runme.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -it --network=softwareplumbers.net --sysctl net.ipv6.conf.all.disable_ipv6=1 -v/tmp/db2:/var/db2 --name gradle1 --hostname=gradle1.softwareplumbers.net softwareplumbers/gradle-centos
+docker run $* /
+    -it /
+    --network=softwareplumbers.net /
+    --sysctl net.ipv6.conf.all.disable_ipv6=1 /
+    -v/tmp/db2:/var/db2 /
+    --name gradle1 /
+    --hostname=gradle1.softwareplumbers.net /
+    softwareplumbers/gradle-centos
