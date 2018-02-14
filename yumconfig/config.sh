59ee07561ca0f60fa30bcc6166a6fb2ce652529f
J.ESSEX <J.ESSEX@TRF52604J0.ATTIJARIWAFA.net>
2018-02-14 09:22:15 +0000

    CR/LF

--- yumconfig/config.sh
+++ yumconfig/config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# configure yum to use a named local repository
 if [[ $1 ]]
 then
 	sed -e 's/LOCAL_REPO_SERVER/'$1'/g' /opt/yumconfig/Local-Centos-Base.repo > /etc/yum.repos.d/Local-Centos-Base.repo
