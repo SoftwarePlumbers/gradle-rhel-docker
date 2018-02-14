bbbf52ce573aedba089912960952d19e07d84189
J.ESSEX <J.ESSEX@TRF52604J0.ATTIJARIWAFA.net>
2018-02-14 09:41:24 +0000

    Corrected path to default CentoOS-Base repo

--- yumconfig/config.sh
+++ yumconfig/config.sh
@@ -3,5 +3,5 @@
 if [[ $1 ]]
 then
 	sed -e 's/LOCAL_REPO_SERVER/'$1'/g' /opt/yumconfig/Local-Centos-Base.repo > /etc/yum.repos.d/Local-Centos-Base.repo
-	rm /etc/yum.repos.d/Centos-Base.repo
+	rm /etc/yum.repos.d/CentOS-Base.repo
 fi
