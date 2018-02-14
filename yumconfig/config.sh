663b08be4e8847446d2e9ef0a8d0701051a5195b
J.ESSEX <J.ESSEX@TRF52604J0.ATTIJARIWAFA.net>
2018-02-14 10:58:35 +0000

    Additional config for local epel repository proxy

--- yumconfig/config.sh
+++ yumconfig/config.sh
@@ -4,4 +4,7 @@ if [[ $1 ]]
 then
 	sed -e 's/LOCAL_REPO_SERVER/'$1'/g' /opt/yumconfig/Local-Centos-Base.repo > /etc/yum.repos.d/Local-Centos-Base.repo
 	rm /etc/yum.repos.d/CentOS-Base.repo
+	sed -e 's/LOCAL_REPO_SERVER/'$1'/g' /opt/yumconfig/Local-epel.repo > /etc/yum.repos.d/Local-epel.repo
+else
+	yum install -y epel-release
 fi
